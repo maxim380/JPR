07101e2f9c5fe12693bbee94ab8270070739d46c
Maxim Doolaard <maximdoolaard@hotmail.com>
2018-05-04 18:21:13 +0200

    Streaming of 1 station now works

--- app/src/main/java/com/maxim/jpr/Util/StationSupplier.java
+++ app/src/main/java/com/maxim/jpr/Util/StationSupplier.java
@@ -8,7 +8,7 @@ public class StationSupplier {
 
     public static ArrayList<Station> getStations() {
         ArrayList<Station> stations = new ArrayList<Station>();
-        stations.add(new Station("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg", "test", "tessstttt", null));
+        stations.add(new Station("http://sky1.torontocast.com:9029/stream/1/?cb=500961.mp3", "J-Pop Powerplay Kawaii", "Kawaiiiiiiii", null));
 
         return stations;
     }
